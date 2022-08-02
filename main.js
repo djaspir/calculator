4da14c3b00c4a5ed0a2469f0522b92a6f6bd6396
djaspir <jaspersanchez1996@gmail.com>
2022-08-02 17:24:48 +0800

    Add display number functionality

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const number = document.querySelectorAll("[data-number]")
+const operator = document.querySelectorAll("[data-operator]")
+const screenBottom = document.getElementById("screen-bottom")
+
+// Num Click Handler
+const handleNumClick = (e) => {
+    displayBottom(e.target.textContent)
+}
+
 // 4 Basic Calculator Function
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -11,3 +20,17 @@ const operate = (operator, num1, num2) => {
             : (operator === "multiplication") ? multiply(num1, num2)
                 : divide(num1, num2)
 }
+
+// Display Function
+const displayBottom = num => {
+    screenBottom.textContent += num;
+    if (screenBottom.textContent.indexOf(0) === 0) {
+        return screenBottom.textContent = screenBottom.textContent
+            .split("").slice(1).join("");
+    }
+}
+
+
+// Add event listener to number element
+number.forEach(elem => elem.addEventListener("click", handleNumClick))
+
