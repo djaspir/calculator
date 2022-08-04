0e328a4508860d8ade39aa946e2a31dbdaac18b9
djaspir <jaspersanchez1996@gmail.com>
2022-08-04 09:26:35 +0800

    Add four basic math function

---
+++ main.js
@@ -0,0 +1,5 @@
+const add = (number1, number2) => number1 + number2;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
+
