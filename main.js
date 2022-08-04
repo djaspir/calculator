1a1ed1b3d1ff450b3b73aa7031d673d9054c230d
djaspir <jaspersanchez1996@gmail.com>
2022-08-04 09:30:19 +0800

    Operate math function

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+// 4 basic math functions
 const add = (number1, number2) => number1 + number2;
 const subtract = (number1, number2) => number1 - number2;
 const multiply = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => number1 / number2;
 
+// Operate math function
+const operate = (operator, number1, number2) => {
+    return (operator === "add") ? add(number1, number2)
+        : (operator === "subtract") ? subtract(number1, number2)
+            : (operator === "multiply") ? multiply(number1, number2)
+                : divide(number1, number2)
+}
